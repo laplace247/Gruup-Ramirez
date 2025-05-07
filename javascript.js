c5708a2e164e5ac2b95a13ce48fb8197f8464cec
Omar Flores <floresquispeomar8@gmail.com>
2025-05-07 02:30:28 -0500

    fix imagenes del page

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-    // Inicializar el slider
-    $("#carouselExample").carousel();
-  
-    // Cambiar en automatico las img cada 3 segs
-    setInterval(function () {
-      $("#carouselExample").carousel("next"); // Cambiar a la siguiente imagen
-    }, 3000); // 3000 milisegundos = 3 segundos
+    // Inicializar el slider con opciones
+    $("#carouselExample").carousel({
+        interval: 3000,
+        pause: 'hover',
+        wrap: true
   });
-  
+});
